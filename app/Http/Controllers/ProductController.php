099e371dab236eaafdc2e77d8f98dbd5b4c5804f
hagan23 <emmanuel.hagan@amalitech.org>
2022-06-12 22:44:31 +0000

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,7 +9,7 @@ class ProductController extends Controller
 {
     public function index()
     {
-        return Product::all();
+        return product::with('station')->get();
     }
 
     public function store(Request $request)
