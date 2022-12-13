87b7afb433705adb84ff265b2d5f8ac9f6982ea2
hagan23 <emmanuel.hagan@amalitech.org>
2022-12-13 15:08:26 +0000

    Create Book.php

---
+++ app/Models/Book.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
+}
